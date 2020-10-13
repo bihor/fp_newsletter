46888e242e3235aa2f8149b0ba31e630ee32b2cd
Kurt Gusbeth <news@quizpalme.de>
2020-10-13 17:15:18 +0200

    version 1.1.4
    Bugfix for TYPO3 10

--- Classes/Controller/LogController.php
+++ Classes/Controller/LogController.php
@@ -880,16 +880,31 @@ class LogController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
         
     	/** @var $message \TYPO3\CMS\Core\Mail\MailMessage */
     	$message = $this->objectManager->get('TYPO3\\CMS\\Core\\Mail\\MailMessage');
-    	$message->setTo($recipient);
-    	$message->setFrom($sender)
-    	->setSubject($subject);
-    
-    	// Plain text example
-    	$message->setBody($emailBodyText, 'text/plain');
-    
-    	// HTML Email
-    	$message->addPart($emailBodyHtml, 'text/html');
-    
+    	if (TYPO3_branch == '9.5') {
+        	$message->setTo($recipient);
+        	$message->setFrom($sender)
+        	   ->setSubject($subject);
+        	$message->setBody($emailBodyText, 'text/plain');
+        	$message->addPart($emailBodyHtml, 'text/html');
+    	} else {
+    	    foreach ($recipient as $key => $value) {
+    	        $email = $key;
+    	        $name = $value;
+    	    }
+    	    $message->to(
+    	        new \Symfony\Component\Mime\Address($email, $name)
+    	    );
+    	    foreach ($sender as $key => $value) {
+    	        $email = $key;
+    	        $name = $value;
+    	    }
+    	    $message->from(
+    	        new \Symfony\Component\Mime\Address($email, $name)
+    	    );
+    	    $message->subject($subject);
+    	    $message->text($emailBodyText);
+    	    $message->html($emailBodyHtml);
+    	}
     	$message->send();
     	return $message->isSent();
     }
