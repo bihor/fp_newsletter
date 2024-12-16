465ae376ad21643369cd18891ed6da3885f18b00
Kurt Gusbeth <news@quizpalme.de>
2024-12-16 10:35:48 +0100

    Version 7.1.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,10 +17,10 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Kurt Gusbeth',
     'author_company' => 'fixpunkt für digitales GmbH',
     'state' => 'stable',
-    'version' => '7.1.0',
+    'version' => '7.1.1',
     'constraints' => [
         'depends' => [
-        	'typo3' => '12.4.0-13.4.99'
+        	'typo3' => '12.4.0-12.4.99'
         ],
         'conflicts' => [],
         'suggests' => [],
