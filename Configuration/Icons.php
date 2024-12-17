289f936fedf0f8c6cca8ef7fe4f4ce370fbd60d9
Kurt Gusbeth <news@quizpalme.de>
2024-12-17 17:10:02 +0100

    Version 8.0.0

---
+++ Configuration/Icons.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'ext-fpnewsletter-wizard-icon' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+        'source' => 'EXT:fp_newsletter/Resources/Public/Icons/Extension.gif'
+    ],
+    'fp_newsletter-plugin-pi1' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+        'source' => 'EXT:fp_newsletter/Resources/Public/Icons/fp_newsletter-plugin.png'
+    ]
+];
--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -7,8 +7,9 @@ foreach (['new', 'form', 'subscribeext', 'verify', 'editemail', 'edit', 'unsubsc
         'FpNewsletter',
         ucfirst($plugin),
         'LLL:EXT:fp_newsletter/Resources/Private/Language/locallang_be.xlf:template.' . $plugin,
-        'EXT:fp_newsletter/Resources/Public/Icons/fp_newsletter-plugin.png',
-        'fp_newsletter'
+        'fp_newsletter-plugin-pi1',
+        'fp_newsletter',
+        'LLL:EXT:fp_newsletter/Resources/Private/Language/locallang_db.xlf:tx_fp_newsletter_domain_model_pi1.description'
     );
 
     $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['fpnewsletter_' . $plugin] = 'pi_flexform';
